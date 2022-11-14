5f7c1f84f1663fd46cba27ef2c20eb5571cb7d57
yvan <yvanpardoalcocer@gmail.com>
2022-11-14 11:44:51 -0300

    express creando rutas

---
+++ M4 unidad3/app.js
@@ -0,0 +1,58 @@
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+var indexRouter = require('./routes/index');
+var productosRouter = require('./routes/productos');
+var clasesRouter = require('./routes/clases');
+var contactoRouter = require('./routes/contacto');
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'hbs');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/productos', productosRouter);
+app.use('/clases', clasesRouter);
+app.use('/contacto', contactoRouter);
+
+
+app.get('/productos', function (req,res) {
+  res.send('Soy la pagina de PRODUCTOS')
+})
+
+app.get('/clases', function(req,res) {
+  res.send('Soy la pagina de CLASES')
+})
+
+app.get('/contacto', function(req,res) {
+  res.send('Soy la pagina de CONTACTO')
+})
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
